a67bab75efd5e4ed5696290b682466fd94bb8a0d
14EC002 <govindhpavi@gmail.com>
2017-07-20 13:57:17 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class sample{
+public static void main(String args[]){
+String name=new String();
+String number="10";
+System.out.println(number);
+}}
